c6f006bb3cb3aceb1d251f6fd942b11f699841a1
Rares Tant <tantrares@yahoo.com>
2023-01-01 21:13:37 +0200

    Adding table with employees

--- Admin_view/index.js
+++ Admin_view/index.js
@@ -14,78 +14,26 @@ axios
       let newOption = new Option(element.name, "Option Value");
       select.add(newOption, undefined);
     });
+
+    CreateTable(data);
   });
 
-export default {
-  data() {
-    return {
-      inputs: [],
-      searchInput: "",
-    };
-  },
-  methods: {
-    openModal() {
-      var modal = document.getElementById("myModal");
-      modal.style.display = "block";
-      window.onclick = function (event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      };
-    },
-    getData() {
-      axios
-        .get("https://localhost:44380/Home/GetTeam")
-        .then((response) => {
-          return response.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-        .then((data) => {
-          this.inputs = data;
-        });
-    },
-    deleteMember(id) {
-      console.log(id);
-      axios.delete(`https://localhost:44380/Home/DeleteMember?id=${id}`);
-      location.reload();
-    },
-    searchMember() {
-      axios
-        .get(`https://localhost:44380/Home/FindMember?name=${this.searchInput}`)
-        .then((response) => {
-          return response.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-        .then((data) => {
-          this.inputs = data;
-        });
-    },
-    RefactorDate(unixDate) {
-      var date = new Date(unixDate * 1000);
-      var year = date.getFullYear();
-      var month = date.getMonth() + 1;
-      var day = date.getDate();
-      var namedMonth;
-      if (month == 1) namedMonth = " January ";
-      if (month == 2) namedMonth = " February ";
-      if (month == 3) namedMonth = " March ";
-      if (month == 4) namedMonth = " April ";
-      if (month == 5) namedMonth = " May ";
-      if (month == 6) namedMonth = " June ";
-      if (month == 7) namedMonth = " July ";
-      if (month == 8) namedMonth = " August ";
-      if (month == 9) namedMonth = " September ";
-      if (month == 10) namedMonth = " October ";
-      if (month == 11) namedMonth = " November ";
-      if (month == 12) namedMonth = " December ";
-      return day + namedMonth + year;
-    },
-  },
-  mounted() {
-    this.getData();
-  },
-};
+function CreateTable(memberList) {
+  memberList.forEach((element) => {
+    InsertElementInTable(element);
+  });
+}
+
+function InsertElementInTable(element) {
+  var table = document.getElementById("table");
+  var row = table.insertRow();
+  row.className = "bottom-row";
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  cell1.innerHTML = element.id;
+  cell2.innerHTML = element.name;
+  cell3.innerHTML = element.hourlyRate;
+  cell4.innerHTML = element.enrollDate;
+}
