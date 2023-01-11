f684174b92af6ccc66df865264de68ee3a1a3094
Rares Tant <tantrares@yahoo.com>
2023-01-11 17:38:05 +0200

    Final functionality for emplyee (js)

---
+++ Employee_view/index.js
@@ -0,0 +1,31 @@
+var checkedIn = false;
+
+document.getElementById("input").addEventListener("click", async function (e) {
+  document.getElementById("input").value = "";
+});
+
+document.getElementById("button").addEventListener("click", async function (e) {
+  if (checkedIn) {
+    document.getElementById("check-out").innerHTML = moment().format(
+      "MMMM Do YYYY, h:mm:ss a"
+    );
+    document.getElementById("buttonReset").disabled = false;
+    checkedIn = false;
+  } else {
+    document.getElementById("button").innerHTML = "Check-out";
+    document.getElementById("check-in").innerHTML = moment().format(
+      "MMMM Do YYYY, h:mm:ss a"
+    );
+    document.getElementById("check-out").innerHTML = "Didn't checked out";
+    checkedIn = true;
+  }
+});
+
+document
+  .getElementById("buttonReset")
+  .addEventListener("click", async function (e) {
+    document.getElementById("button").innerHTML = "Check-in";
+    document.getElementById("check-in").innerHTML = "Didn't checked in";
+    document.getElementById("check-out").innerHTML = "Can't check out";
+    document.getElementById("buttonReset").disabled = true;
+  });
