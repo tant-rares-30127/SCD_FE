363d2d06bc348c6b4d5538f99a98979ad0c3e6eb
Rares Tant <tantrares@yahoo.com>
2023-01-13 00:23:58 +0200

    Working saving check-out

--- Employee_view/index.js
+++ Employee_view/index.js
@@ -1,4 +1,5 @@
 var checkedIn = false;
+var checkin;
 
 document.getElementById("input").addEventListener("click", async function (e) {
   document.getElementById("input").value = "";
@@ -11,11 +12,17 @@ document.getElementById("button").addEventListener("click", async function (e) {
     );
     document.getElementById("buttonReset").disabled = false;
     checkedIn = false;
+    var id = document.getElementById("input").value;
+    axios.post(`http://localhost:8083/employeeHours/${id}`, {
+      checkIn: checkin,
+      checkOut: moment(),
+    });
   } else {
     document.getElementById("button").innerHTML = "Check-out";
     document.getElementById("check-in").innerHTML = moment().format(
       "MMMM Do YYYY, h:mm:ss A"
     );
+    checkin = moment();
     document.getElementById("check-out").innerHTML = "Didn't checked out";
     checkedIn = true;
   }
