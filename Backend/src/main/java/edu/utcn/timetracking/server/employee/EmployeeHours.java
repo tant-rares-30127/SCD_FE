f155560ad29b32b483fa96df246d14d36422c42a
Rares Tant <tantrares@yahoo.com>
2023-01-12 09:53:18 +0200

    Adding getters and setter

--- Backend/src/main/java/edu/utcn/timetracking/server/employee/EmployeeHours.java
+++ Backend/src/main/java/edu/utcn/timetracking/server/employee/EmployeeHours.java
@@ -33,4 +33,36 @@ public class EmployeeHours {
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private Employee employee;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 }
